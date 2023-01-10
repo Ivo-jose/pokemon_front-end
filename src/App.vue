091d6fa5875aab5f61dbfd95299c0009630d942d
Ivo Jose <ivojmgs19@gmail.com>
2023-01-10 10:08:58 -0300

    Requisicao automatica para PokeAPI

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,18 @@
 <script>
 import axios from 'axios';
 export default {
-  name: 'App'
+  name: 'App',
+  data() {
+    return {
+      pokemons: [],
+    }
+  },
+  created: function() {
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0').then(res => {
+      console.log('Pegou a lista de pokemon');
+      this.pokemons = res.data.results;   
+    })
+  }
 }
 </script>
 
