1d584250a2f21b131b5fb602e3dbd30bf18fefa2
Ivo Jose <ivojmgs19@gmail.com>
2023-01-16 10:15:39 -0300

    Invertendo imagens

--- src/components/Pokemon.vue
+++ src/components/Pokemon.vue
@@ -6,7 +6,7 @@
         <div class="card-image has-background-primary-light">
             <div id="imagem">
                 <figure>
-                    <img :src="pokemon.front" alt="Placeholder image">
+                    <img :src="currentImgFront" alt="Placeholder image">
                 </figure>
             </div>
         </div>
@@ -14,18 +14,18 @@
             <div class="media">
                 <div class="media-left has-background-primary-light">
                     <figure class="image is-96x96">
-                    <img :src="pokemon.back" alt="Placeholder image">
+                    <img :src="currentImgBack" alt="Placeholder image">
                     </figure>
                 </div>
                 <div class="media-content">
                     <p class="title is-4">{{ upper(name) }}</p>
-                    <p class="subtitle is-4 mb-1"><strong>Tipo:</strong> {{ pokemon.type }}</p>
+                    <p class="subtitle is-4 mb-1"><strong>Tipo principal:</strong> {{ pokemon.type }}</p>
                     <p class="subtitle is-6"><strong>Url:</strong> {{ url }}</p>
                 </div>
             </div>
 
             <div class="content">
-                
+                <button @click="mudarSprite" class="button is-danger is-rounded">Inverter imagem</button>
             </div>
         </div>
         <hr>
@@ -42,11 +42,17 @@
                 this.pokemon.type = res.data.types[0].type.name;
                 this.pokemon.front = res.data.sprites.front_default;
                 this.pokemon.back = res.data.sprites.back_default; 
+                this.currentImgFront = this.pokemon.front;
+                this.currentImgBack = this.pokemon.back
                 console.log(this.pokemon);
             })
         },
         data() {
             return {
+                isFront: true,
+                isBack: true,
+                currentImgFront: "",
+                currentImgBack:"",
                 pokemon: {
                     type: '',
                     front: '',
@@ -64,7 +70,22 @@
         methods: {
             upper: function (value) {
                 return `${value[0].toUpperCase()}${value.slice(1)}`;
-            }
+            },
+
+            mudarSprite: function() {
+                if(this.isFront && this.isBack) {
+                    this.isFront = false;
+                    this.isBack = false;
+                    this.currentImgFront = this.pokemon.back;
+                    this.currentImgBack = this.pokemon.front;
+                }
+                else {
+                    this.isFront = true;
+                    this.isBack = true;
+                    this.currentImgFront = this.pokemon.front;
+                    this.currentImgBack = this.pokemon.back;
+                }
+            },  
         }
     }
 </script>
@@ -82,5 +103,10 @@ img {
     height: 310px;
     width: 310px;
     padding: 5px;
+}
+
+button:hover {
+    font-weight: bold;
+    box-shadow: 4px 4px 6px rgb(42, 42, 42);
 }
 </style>
